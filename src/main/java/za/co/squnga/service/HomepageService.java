68016801eb0f698b6503a6ade88f6c5a0d64d846
Noxolo Mandisa Mkhungo <NMandisa@users.noreply.github.com>
2023-11-07 08:01:27 +0200

    ...

--- src/main/java/za/co/squnga/service/HomepageService.java
+++ src/main/java/za/co/squnga/service/HomepageService.java
@@ -3,7 +3,6 @@ package za.co.squnga.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import za.co.squnga.controller.HomePageController;
 
 /**
  * @author Noxolo.Mkhungo
--- src/main/java/za/co/squnga/web/WebInitializer.java
+++ src/main/java/za/co/squnga/web/WebInitializer.java
@@ -2,7 +2,6 @@ package za.co.squnga.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import za.co.squnga.controller.HomePageController;
 
 /**
  * @author Noxolo.Mkhungo
