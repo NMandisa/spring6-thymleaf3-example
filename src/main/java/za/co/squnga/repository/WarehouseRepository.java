24beb8bef831b6aca8afc4295f854708764186c8
Noxolo Mandisa Mkhungo <NMandisa@users.noreply.github.com>
2023-11-20 01:20:56 +0200

    Spring Data added

---
+++ src/main/java/za/co/squnga/repository/WarehouseRepository.java
@@ -0,0 +1,11 @@
+package za.co.squnga.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.co.squnga.entity.Warehouse;
+
+/**
+ * @author Noxolo.Mkhungo
+ */
+public interface WarehouseRepository extends JpaRepository<Warehouse,Long> {
+
+}
