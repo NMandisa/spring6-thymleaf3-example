57126c5cd53a198f5f34927447e57d2902313852
Noxolo Mandisa Mkhungo <NMandisa@users.noreply.github.com>
2023-11-20 09:51:13 +0200

    Moved harded coded values to properties file

--- src/main/java/za/co/squnga/repository/WarehouseRepository.java
+++ src/main/java/za/co/squnga/repository/WarehouseRepository.java
@@ -1,11 +1,15 @@
 package za.co.squnga.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import za.co.squnga.entity.Warehouse;
+
+import java.util.List;
 
 /**
  * @author Noxolo.Mkhungo
  */
 public interface WarehouseRepository extends JpaRepository<Warehouse,Long> {
-
+    List<Warehouse> findAll();
 }
