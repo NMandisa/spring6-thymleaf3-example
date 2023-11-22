a417903b7fe45d3de45f02dde8028ae46c78a43d
Noxolo Mandisa Mkhungo <NMandisa@users.noreply.github.com>
2023-11-22 02:54:22 +0200

    renamed

--- src/main/java/za/co/squnga/facade/HomPageFacade.java
+++ src/main/java/za/co/squnga/facade/HomPageFacade.java
@@ -2,13 +2,11 @@ package za.co.squnga.facade;
 
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 /**
  * @author Noxolo.Mkhungo
  */
 @Component
-public class WarehouseFacade {
+public class HomPageFacade {
 
 
 }
