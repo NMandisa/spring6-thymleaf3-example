256ed1f35cccad91dde09300e09c21e456b43a18
Noxolo Mandisa Mkhungo <NMandisa@users.noreply.github.com>
2023-11-27 22:21:48 +0200

    Added Exception Handler Validator

--- src/main/java/za/co/squnga/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/za/co/squnga/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,7 +1,10 @@
 package za.co.squnga.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -9,6 +12,8 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import za.co.squnga.dto.ErrorResponseDTO;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
 @ControllerAdvice
@@ -22,18 +27,28 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
         return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);//500
     }
     @ExceptionHandler(ProductNotFoundException.class)
-    public final ResponseEntity<ErrorResponseDTO> handleProductNotFoundException(Exception ex, WebRequest request) throws Exception {
+    public final ResponseEntity<ErrorResponseDTO> handleProductNotFoundException(Exception ex, WebRequest request){
         ErrorResponseDTO errorDetails = new ErrorResponseDTO(LocalDateTime.now(),
                 ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);//404
     }
 
     @ExceptionHandler(ProductAlreadyExistsException.class)
-    public final ResponseEntity<ErrorResponseDTO> handleProductAlreadyExistsException(Exception ex, WebRequest request) throws Exception {
+    public final ResponseEntity<ErrorResponseDTO> handleProductAlreadyExistsException(Exception ex, WebRequest request) {
         ErrorResponseDTO errorDetails = new ErrorResponseDTO(LocalDateTime.now(),
                 ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.CONFLICT);//409
 
+    }
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) ->{
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+        return new ResponseEntity<Object>(errors, HttpStatus.BAD_REQUEST);//400
     }
 
 }
