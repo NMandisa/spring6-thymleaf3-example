1740d672bc43d0d85aff7fae4f38c8c852a64473
Noxolo Mandisa Mkhungo <NMandisa@users.noreply.github.com>
2023-12-10 01:57:47 +0200

    Error Controller in progress

---
+++ src/main/java/za/co/squnga/controller/ErrorController.java
@@ -0,0 +1,38 @@
+package za.co.squnga.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author Noxolo.Mkhungo
+ */
+@Controller
+public class ErrorController {
+    @RequestMapping(value = "errors", method = RequestMethod.GET)
+    public ModelAndView renderErrorPage(HttpServletRequest httpRequest) {
+
+        ModelAndView errorPage = new ModelAndView("/error");
+        String errorMsg = "";
+        int httpErrorCode = getErrorCode(httpRequest);
+        switch (httpErrorCode) {
+            case 404: {
+                errorPage = new ModelAndView("404");
+                errorMsg = "Http Error Code: 404. Resource not found";
+                break;
+            }
+            case 500: {
+                errorPage = new ModelAndView("500");
+                errorMsg = "Http Error Code: 500. Internal Server Error";
+                break;
+            }
+        }
+        errorPage.addObject("errorMsg", errorMsg);
+        return errorPage;
+    }
+    private int getErrorCode(HttpServletRequest httpRequest) {
+        return (Integer) httpRequest.getAttribute("jakarta.servlet.error.status_code");
+    }
+}
