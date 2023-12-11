72c4458a9f67dcb48bd74af9eadb59835e7cb539
Noxolo Mandisa Mkhungo <NMandisa@users.noreply.github.com>
2023-12-11 14:51:12 +0200

    URI Constants

--- src/main/java/za/co/squnga/web/WebRestURIConstants.java
+++ src/main/java/za/co/squnga/web/WebRestURIConstants.java
@@ -5,8 +5,8 @@ package za.co.squnga.web;
  */
 public class WebRestURIConstants {
     public static final String PRODUCTS_REST_MAPPING = "/rest/products";
-    public static final String GET_ALL_PRODUCTS = PRODUCTS_REST_MAPPING+"/";
-    public static final String GET_PRODUCT = PRODUCTS_REST_MAPPING+"/{id}";
-    public static final String CREATE_PRODUCT = PRODUCTS_REST_MAPPING+"/create";
-    public static final String DELETE_PRODUCT = PRODUCTS_REST_MAPPING+"/delete/{id}";
+    public static final String GET_ALL_PRODUCTS = "/rest/products/";
+    public static final String GET_PRODUCT = "/rest/products/{id}";
+    public static final String CREATE_PRODUCT = "/rest/products/create";
+    public static final String DELETE_PRODUCT = "/rest/products/delete/{id}";
 }
